8ea695da23d040be93871d09a9bf1dd374c3de29
Martin Hong <hongzeqin@gmail.com>
2015-12-12 00:48:59 +0800

    enable to load bitmap

--- src/watchface.c
+++ src/watchface.c
@@ -3,6 +3,8 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -20,6 +22,13 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 
+  // 添加位图
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  // 创建文本层
   s_time_layer = text_layer_create(
     GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
@@ -34,6 +43,8 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   fonts_unload_custom_font(s_time_font);
   text_layer_destroy(s_time_layer);
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -47,6 +58,7 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
+  window_set_background_color(s_main_window, GColorBlack);
 
   window_stack_push(s_main_window, true);
   update_time();
