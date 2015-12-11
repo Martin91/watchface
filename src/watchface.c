6a9c060d0000e42e6536d7ef328fcd895107c03c
Martin Hong <hongzeqin@gmail.com>
2015-12-12 00:36:12 +0800

    introduce custom font

--- src/watchface.c
+++ src/watchface.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -17,19 +18,21 @@ static void update_time() {
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 
   s_time_layer = text_layer_create(
     GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
+  fonts_unload_custom_font(s_time_font);
   text_layer_destroy(s_time_layer);
 }
 
